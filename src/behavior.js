1556a47ec81305c06c9b35e138f2f60c6756eb75
Johannes Stein <johannesstein@freeze-dev.com>
2015-01-08 22:52:33 +0100

    First draft

--- src/behavior.js
+++ src/behavior.js
@@ -1,9 +1,26 @@
 define(function() {
 
-  var Behavior = function() {
+  var Behavior = function(target, descriptor) {
+    this.target = target;
+    this.name = 'undefined-behavior';
 
+    descriptor.call(this);
   };
 
 
+  Behavior.list = {};
+
+  Behavior.add = function(name, descriptor) {
+    Behavior.list[name] = function(target) {
+      var behavior = new Behavior(target, descriptor);
+      behavior.name = name;
+
+      return behavior;
+    };
+  };
+
+  Behavior.remove = function(name) {
+    Behavior.list[name] = undefined;
+  };
 
 });
