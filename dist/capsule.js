24187c7a35d8be96269ed9ad887ca9c256ee8ac6
Johannes Stein <johannesstein@freeze-dev.com>
2015-01-14 23:00:46 +0100

    Update dist

--- dist/capsule.js
+++ dist/capsule.js
@@ -1,48 +1,97 @@
-(function() {
-  (function(root, $) {
-    return udefine.configure(function(root) {
-      udefine.modules.globals['jquery'] = $;
-      return udefine.inject.add('capsule', {
-        root: root,
-        name: 'Capsule'
-      });
+define('capsule/behavior', function() {
+
+  var Behavior = function(target, descriptor) {
+    this.target = target;
+    this.name = 'undefined-behavior';
+
+    descriptor.call(this);
+  };
+
+
+  Behavior.list = {};
+
+  Behavior.add = function(name, descriptor) {
+    Behavior.list[name] = function(target) {
+      var behavior = new Behavior(target, descriptor);
+      behavior.name = name;
+
+      return behavior;
+    };
+  };
+
+  Behavior.call = function(name, target) {
+    Behavior.list[name](target);
+  };
+
+  Behavior.remove = function(name) {
+    Behavior.list[name] = undefined;
+  };
+
+  return Behavior;
+
+});
+define('capsule', ['jquery', 'eventmap', 'capsule/behavior'], function($, EventMap, Behavior) {
+
+  var Capsule = function(target, factory) {
+    EventMap.mixin(this, Capsule);
+
+    this.$target = (target instanceof $) ? target : $(target);
+    this.factory = factory;
+
+    this.defaultEvent = Capsule.defaultEvent;
+
+    this.localization = new Localization();
+
+    this.id = 'capsule-' + Date.now();
+
+    this.template = '';
+    this.style = '';
+  };
+
+  Capsule.prototype.bindEvents = function(obj) {
+    Object.keys(obj).forEach(function(name) {
+      var eventName = name.split(' ')[0];
+      var eventDelegate = name.split(' ')[1];
+      var event = obj[name];
+
+      $target.on(eventName, eventDelegate, event);
+    }, this);
+  };
+
+  Capsule.prototype.addBehavior = function(name) {
+    $('[data-behavior=' + name + ']', this.$target).each(function(t) {
+      var $t = $(t);
+      Behavior.call(name, $t);
     });
-  })(this, this.jQuery);
+  };
 
-}).call(this);
+  Capsule.prototype.execute = function(args) {
+    this.factory.apply(this, args);
 
-(function() {
-  udefine('capsule', ['jquery', 'mixedice', 'eventmap'], function($, mixedice, EventMap) {
-    var Capsule;
-    Capsule = (function() {
-      function Capsule(fn, elem) {
-        var _ref,
-          _this = this;
-        mixedice([this, Capsule.prototype], new EventMap());
-        if (fn instanceof $) {
-          _ref = [null, fn], fn = _ref[0], elem = _ref[1];
-        }
-        if (fn != null) {
-          fn.call(this, this);
-        }
-        this.data = {};
-        this.on('render', function() {
-          if (!_this.template) {
-            return;
-          }
-          return _this.template(_this.data);
-        });
-      }
+    // Inject style
+    var completeStyle = '<style id="' + this.id + '">' + $target.selector + '{ ' + this.style + ' } </style>';
 
-      Capsule.prototype.execute = function() {
-        return this.trigger.apply(this, ['render', arguments]);
-      };
+    $('head').append(completeStyle);
+  };
 
-      return Capsule;
+  Capsule.prototype.t = function(name) {
 
-    })();
-    Capsule.TemplateConnector = {};
-    return Capsule;
-  });
+  };
 
-}).call(this);
+  Capsule.EventPool = new EventMap();
+
+  Capsule.TemplateConnector = null;
+
+  Capsule.defaultEvent = 'click';
+
+  return Capsule;
+
+});
+define('capsule/localization', function() {
+  var Localization = function(data) {
+    this.data = data || {};
+    this.language = Localization.defaultLanguage;
+  };
+
+  Localization.defaultLanguage = 'en';
+});
